ec8bc5e7e785639c548e3becf376e4a348e31aa3
Stephen Yeargin <stephen.yeargin@gmail.com>
2019-01-06 15:13:18 -0600

    Dummy public content

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
---
+++ src/Controller/PublicController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        $volumes = [];
+        foreach (range(1928, 2019) as $value) {
+            $volumes[] = [
+                'number' => $value - 1927,
+                'yearStart' => $value,
+                'yearEnd' => $value + 1,
+                'issueCount' => 0
+            ];
+        }
+
+        return $this->render('public/index.html.twig', [
+            'volumes' => $volumes
+        ]);
+    }
+}
