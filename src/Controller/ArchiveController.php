bebd8acab021e2408dc959cdb8ceb85d805c3f1d
Stephen Yeargin <stephen.yeargin@gmail.com>
2019-01-09 13:18:06 -0600

    Twig extensions, issue view

--- src/Controller/ArchiveController.php
+++ src/Controller/ArchiveController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Volume;
+use App\Entity\Issue;
 
 class ArchiveController extends AbstractController
 {
@@ -23,16 +24,28 @@ class ArchiveController extends AbstractController
     }
 
     /**
-     * @Route("/volume-{volume_number}", name="volume")
+     * @Route("/volume-{volumeNumber}", name="volume")
      */
-    public function volume(int $volume_number)
+    public function volume(int $volumeNumber)
     {
         $entityManager = $this->getDoctrine()->getManager();
-        $volume = $entityManager->getRepository(Volume::class)->findOneBy(['volumeNumber' => $volume_number]);
+        $volume = $entityManager->getRepository(Volume::class)->findOneBy(['volumeNumber' => $volumeNumber]);
 
         return $this->render('archive/volume.html.twig', [
             'volume' => $volume
         ]);
     }
 
+    /**
+     * @Route("/issue-{archiveKey}", name="issue")
+     */
+    public function issue(string $archiveKey)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $issue = $entityManager->getRepository(Issue::class)->findOneBy(['archiveKey' => $archiveKey]);
+
+        return $this->render('archive/issue.html.twig', [
+            'issue' => $issue
+        ]);
+    }
 }
