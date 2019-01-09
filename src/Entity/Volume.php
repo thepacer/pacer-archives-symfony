097f62ad21292320f92ad502d1b00485d9dd1904
Stephen Yeargin <stephen.yeargin@gmail.com>
2019-01-09 09:02:57 -0600

    Use ArrayCollection for property

--- src/Entity/Volume.php
+++ src/Entity/Volume.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -45,7 +46,7 @@ class Volume
 
     public function __construct()
     {
-        $this->issues = new Collection();
+        $this->issues = new ArrayCollection();
     }
 
     public function getId(): ?int
