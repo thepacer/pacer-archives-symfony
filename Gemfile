26537a3f6ed89246f7c948d2ea8767e7418b0fcd
Stephen Yeargin <stephen.yeargin@gmail.com>
2019-01-21 19:48:29 -0600

    Capistrano updates

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem 'capistrano',  '~> 3.11'
+gem 'capistrano', '~> 3.11'
+gem 'capistrano-composer'
 gem 'capistrano-symfony', '~> 2.0.0.pre.alfa2'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
+set :deploy_to, "/srv/www/thepacer.org/production"
+
+server "thepacer.org", user: "deploy", roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
