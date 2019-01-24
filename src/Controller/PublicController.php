133fe33b33c3f92784a34be7ed7118804806d1d4
Stephen Yeargin <stephen.yeargin@gmail.com>
2019-01-23 18:37:41 -0600

    Ensure article route locked

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Cache\Simple\FilesystemCache;
 
+use Symfony\Component\HttpFoundation\Request;
+
 use App\Entity\Issue;
 use GuzzleHttp\Client as Client;
 
@@ -14,24 +16,28 @@ class PublicController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
         $cache = new FilesystemCache();
         $entityManager = $this->getDoctrine()->getManager();
 
+        // Clear cache
+        if ($request->get('clearCache')) {
+            $cache->clear();
+        }
+
         if (!$cache->has('public.pacer_site_feed')) {
             $client = new Client();
             $response = $client->get('http://www.thepacer.net/wp-json/wp/v2/posts?_embed&per_page=5');
-            $feed = json_decode($response->getBody());
-            $cache->set('public.pacer_site_feed', $feed);
+            $cache->set('public.pacer_site_feed', json_decode($response->getBody()), 3600);
         }
 
-        if (!$cache->has('public.archived_issue_count')) {
-            $cache->set('public.archived_issue_count', $entityManager->getRepository(Issue::class)->countIssues());
+        if (!$cache->has('public.issue_count')) {
+            $cache->set('public.issue_count', $entityManager->getRepository(Issue::class)->countIssues(), 300);
         }
 
         $feed = $cache->get('public.pacer_site_feed');
-        $issue_count = $cache->get('public.archived_issue_count');
+        $issue_count = $cache->get('public.issue_count');
 
         return $this->render('public/index.html.twig', [
             'feed' => $feed,
