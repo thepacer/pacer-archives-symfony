d8609ee61a906243672e9878252ef608eaa0211f
Stephen Yeargin <stephen.yeargin@gmail.com>
2020-10-26 23:51:09 -0500

    Fix tests

--- src/DataFixtures/VolumeFixtures.php
+++ src/DataFixtures/VolumeFixtures.php
@@ -3,7 +3,7 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 use App\Entity\Volume;
 use App\Entity\Issue;
