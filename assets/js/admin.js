e4473906eeee7451951d3b0a556413d82c1a0a13
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-05-12 15:57:20 -0500

    Fix javascript

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,22 +1,21 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+/* jshint esversion: 6 */
 
 // Javascript
-const $ = require('jquery')
-global.$ = global.jQuery = $
-require('popper.js')
-require('bootstrap')
-require('clndr')
-require('../../node_modules/datatables.net/js/jquery.dataTables.js')
-require('../../node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js')
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+import { createPopper } from '@popperjs/core';
+const bootstrap = require('bootstrap');
+import { Tooltip, Toast, Popover } from 'bootstrap';
+require('clndr');
+require('../../node_modules/datatables.net/js/jquery.dataTables.js');
+require('../../node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js');
 
 // CSS
-require('../../node_modules/bootstrap/dist/css/bootstrap.css')
-require('../../node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css')
-require('../css/admin.scss')
+require('../../node_modules/bootstrap/dist/css/bootstrap.css');
+require('../../node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css');
+require('../css/admin.scss');
 
-$('[data-toggle="tooltip"]').tooltip();
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,19 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+/* jshint esversion: 6 */
 
 // Javascript
-const $ = require('jquery')
-global.$ = global.jQuery = $
-require('popper.js')
-require('bootstrap')
-global._ = require('underscore')
-require('clndr')
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+import { createPopper } from '@popperjs/core';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+const bootstrap = require('bootstrap');
+global._ = require('underscore');
+require('clndr');
 
 // CSS
-require('../../node_modules/bootstrap/dist/css/bootstrap.css')
-require('../css/app.scss')
+require('../../node_modules/bootstrap/dist/css/bootstrap.css');
+require('../css/app.scss');
 
-$('[data-toggle="tooltip"]').tooltip();
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
