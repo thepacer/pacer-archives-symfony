d49d2c4c96d5dce8bf023001de25d4781afbb15c
Stephen Yeargin <stephenyeargin@users.noreply.github.com>
2021-06-19 10:12:28 -0500

    Fix images

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ Encore
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
+   .copyFiles({
+      from: './assets/images'
+    })
+
     /*
      * ENTRY CONFIG
      *
