70535788e41232ef7a6142d4bab4cfe872b4060b
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-08-29 14:23:36 -0500

    Remove deprecated twig-extensions package

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('pluralize', [$this, 'getPluralizedString']),
+        ];
+    }
+
+    /**
+     * @url https://github.com/TomodomoCo/twig-pluralize-extension/blob/master/src/Pluralize.php
+     */
+    public function getPluralizedString(int $count, string $one, string $many, string $none = null): string
+    {
+        // If the option for $none is null, use the option for $many
+        $none = $none ?? $many;
+
+        // Choose the correct string
+        switch($count) {
+            case 0:
+                $string = $none;
+                break;
+            case 1:
+                $string = $one;
+                break;
+            default:
+                $string = $many;
+                break;
+        }
+
+        // Return the result
+        return sprintf($string, $count);
+    }
+}
