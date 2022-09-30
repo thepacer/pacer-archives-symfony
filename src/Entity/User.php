185380c2ee07bc8d4b55bcd49ded9532a7e020cd
stephenyeargin <stephenyeargin@users.noreply.github.com>
2022-09-30 21:15:34 +0000

    Apply php-cs-fixer changes as of 00feea6bd6fe36021a289a8f99132b0e541fa415

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -30,6 +30,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @var string The hashed password
+     *
      * @ORM\Column(type="string")
      */
     private $password;
